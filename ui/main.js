4b3563cd0840045055fb5a46fa07e3db26d2e8d1
MuhilanPandian <30727530+MuhilanPandian@users.noreply.github.com>
2017-08-20 13:14:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,20 @@ button.onclick = function () {
         request.open('GET','http://muhilanpandian97.imad.hasura-app.io/counter',true);
         request.send(null);
 };
+
+// Submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    // Make a request to the server and send the name
+    
+    // Capture the list of names and render it as the list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i< names.length; i++) {
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
